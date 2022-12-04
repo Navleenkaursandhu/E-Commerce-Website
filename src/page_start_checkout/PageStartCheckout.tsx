341c038ef5ba6d5d92ebd97aada168f0a3dc112a
Navleen <navleensandhu0211@gmail.com>
2022-12-04 14:47:18 -0800

    fetched bag items inside react component

--- src/page_start_checkout/PageStartCheckout.tsx
+++ src/page_start_checkout/PageStartCheckout.tsx
@@ -1,4 +1,12 @@
+import { useLiveQuery } from "dexie-react-hooks"
+import { db } from "../db"
+import { product } from "../models/product"
+
 export const PageStartCheckout = () => {
+
+  const bagItems = useLiveQuery(() => db.bagItems.toArray())
+  console.log(bagItems)
+
   return (
     <div className="text-[#7D515E]">
       <div>Checkout</div>
