9314cb1a87bbe7e58a5337eb0684e1848f6beaf5
Navleen <navleensandhu0211@gmail.com>
2022-12-07 15:55:20 -0800

    authenticated user login
     - the user with account can login to the website

--- src/page_login/PageLogin.tsx
+++ src/page_login/PageLogin.tsx
@@ -1,8 +1,25 @@
+import { useLiveQuery } from "dexie-react-hooks"
 import { buttonShadowEffect } from '../common/tailwind_constants'
 import girlLoginImage from '../assets/GirlLoginImage.svg'
 import { Header } from '../common/Header'
 import { Footer } from '../common/Footer'
+import { useState } from 'react'
+import { db } from '../db'
 export const PageLogin = () => {
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+  const [isUserValid, setIsUserValid] = useState(false)
+
+  const loginUser = async () => {
+    setIsUserValid(false)
+    const result = await db.user.where({ email: userEmail }).toArray()
+    if (userPassword === result[0].passwordHash && result[0].passwordHash) {
+      setIsUserValid(true)
+    }
+  }
+
+  console.log(isUserValid)
+
   return (
     <div className='text-[#7D515E] flex flex-col min-h-screen'>
       <Header />
@@ -12,17 +29,21 @@ export const PageLogin = () => {
             <div className='text-center font-semibold text-2xl'>LOGIN</div>
             <div className='flex flex-col gap-16 pt-16'>
               <div className='flex flex-col gap-2'>
-                <div>Username</div>
-                <input className='w-full px-4 py-2 rounded-md bg-[#F4DADB]' placeholder='Type your username'></input>
+                <div>E-mail</div>
+                <input onChange={(e) => setUserEmail(e.target.value)} className='w-full px-4 py-2 rounded-md bg-[#F4DADB]' placeholder='Type your e-mail address'></input>
               </div>
               <div className='flex flex-col gap-2'>
                 <div>Password</div>
-                <input className='w-full px-4 py-2 rounded-md bg-[#F4DADB]' placeholder='Type your password'></input>
+                <input onChange={(e) => setUserPassword(e.target.value)} className='w-full px-4 py-2 rounded-md bg-[#F4DADB]' placeholder='Type your password'></input>
                 <div className='flex justify-end'>
                   <div><a href='/forgot-password'>Forgot Password?</a></div>
                 </div>
               </div>
-              <button className={`${buttonShadowEffect} font-semibold shadow-[4px_4px_0px_0px_#B58396] hover:shadow-[2px_2px_0px_0px_#B58396] bg-[#C2ADB3] w-full p-2 rounded-md`}>LOGIN</button>
+              <button onClick={() => loginUser()} className={`${buttonShadowEffect} font-semibold shadow-[4px_4px_0px_0px_#B58396] hover:shadow-[2px_2px_0px_0px_#B58396] bg-[#C2ADB3] w-full p-2 rounded-md`}>LOGIN</button>
+              {isUserValid && <div className="text-center">User Successfully Logged In</div>}
+              {!isUserValid && <div className="flex items-center justify-center gap-2 text-red-800 text-lg"><span className="material-symbols-outlined">
+                warning
+              </span>Oops! Incorrect E-mail or Password</div>}
             </div>
           </div>
         </div>
