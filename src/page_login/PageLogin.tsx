c7c74e54ed08a0fff83b6797403846458e6567de
Navleen <navleensandhu0211@gmail.com>
2022-12-08 10:05:57 -0800

    reimplemented user login error msg using state variables

--- src/page_login/PageLogin.tsx
+++ src/page_login/PageLogin.tsx
@@ -9,27 +9,46 @@ import bcrypt from 'bcryptjs'
 export const PageLogin = () => {
   const [userEmail, setUserEmail] = useState('')
   const [userPassword, setUserPassword] = useState('')
-  const [isUserValid, setIsUserValid] = useState(false)
-  const[hasUserLoggedIn, setHasUserLoggedIn] = useState(false)
+  const [loginSuccessful, setLoginSuccessful] = useState(false)
+  const [loginFailed, setLoginFailed] = useState(false)
+
 
   const loginUser = async () => {
-    setIsUserValid(false)
-    setHasUserLoggedIn(true)
+    setLoginSuccessful(false)
+    setLoginFailed(false)
 
     const result = await db.user.where({ email: userEmail }).toArray()
+
+    if (result.length === 0) {
+      setLoginFailed(true)
+    }
 
     bcrypt.compare(userPassword, result[0].passwordHash, function (err, isMatch) {
       if (err) {
         throw err
       }
+
       else if (!isMatch) {
-        setIsUserValid(false)
+        setLoginFailed(true)
       }
       else {
-        setIsUserValid(true)
+        setLoginSuccessful(true)
       }
     })
   }
+
+  // const LoginButton = () => {
+  //   if(isUserValid && hasUserLoggedIn) {
+
+  //     return <button onClick={() => loginUser()} 
+  //       className={`${buttonShadowEffect} font-semibold shadow-[4px_4px_0px_0px_#B58396] hover:shadow-[2px_2px_0px_0px_#B58396] bg-[#C2ADB3] w-full p-2 rounded-md`}>LOGIN</button>
+  //   }
+  //   else {
+
+  //     return <button onClick={() => loginUser()
+  //     } className={`${buttonShadowEffect} disable font-semibold shadow-[4px_4px_0px_0px_#B58396] hover:shadow-[2px_2px_0px_0px_#B58396] bg-[#C2ADB3] w-full p-2 rounded-md`}>LOGIN</button>
+  //   }
+  // }
 
   return (
     <div className='text-[#7D515E] flex flex-col min-h-screen'>
@@ -51,8 +70,9 @@ export const PageLogin = () => {
                 </div>
               </div>
               <button onClick={() => loginUser()} className={`${buttonShadowEffect} font-semibold shadow-[4px_4px_0px_0px_#B58396] hover:shadow-[2px_2px_0px_0px_#B58396] bg-[#C2ADB3] w-full p-2 rounded-md`}>LOGIN</button>
-              {isUserValid && <div className="text-center">User Successfully Logged In</div>}
-              {!isUserValid && hasUserLoggedIn && <div className="flex items-center justify-center gap-2 text-red-800 text-lg"><span className="material-symbols-outlined">
+              {/* <LoginButton /> */}
+              {loginSuccessful && <div className="text-center">User Successfully Logged In</div>}
+              {loginFailed && <div className="flex items-center justify-center gap-2 text-red-800 text-lg"><span className="material-symbols-outlined">
                 warning
               </span>Oops! Incorrect E-mail or Password</div>}
             </div>
