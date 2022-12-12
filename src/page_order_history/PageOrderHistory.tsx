aceccb69cf25fa213d5af3c99d5dc6c97e3a7fc3
Navleen <navleensandhu0211@gmail.com>
2022-12-11 16:19:41 -0800

    added responsive design to order history page

--- src/page_order_history/PageOrderHistory.tsx
+++ src/page_order_history/PageOrderHistory.tsx
@@ -31,35 +31,44 @@ export const PageOrderHistory = () => {
     <>
       <div className='text-[#7D515E] flex flex-col min-h-screen'>
         <Header />
-        <div className='flex flex-1 lg:flex-row flex-col bg-[#F3EBF1]'>
-          {!!orderPlacedData?.length && orderPlacedData.map((order, i) => {
-            return <div key={order.id}>
-              <div>OrderId: {order.id}</div>
-              <div>Date: {new Date(order.timestamp).toLocaleString()}</div>
-              <div>Total: {CURRENCY} {order.products.map((product) => product.price * product.qty).reduce((prev, curr) => prev + curr)}</div>
-              <div>Status: Processing</div>
+        <div className='flex flex-1 flex-col items-center bg-[#F3EBF1]'>
+          <div className="flex flex-col gap-8 md:p-16 sm:p-10 p-4 lg:w-1/2 w-full">
+            {!!orderPlacedData?.length && orderPlacedData.map((order, i) => {
+              return <div className="rounded-md bg-[#F4DADB] flex flex-col lg:gap-6 gap-12 p-4">
+                <div key={order.id}>
+                  <div>OrderId: {order.id.toString().padStart(5, '0')}</div>
+                  <div>Date: {new Date(order.timestamp).toLocaleString()}</div>
+                  <div>Total: {CURRENCY} {order.products.map((product) => product.price * product.qty).reduce((prev, curr) => prev + curr)}</div>
+                  <div>Status: Processing</div>
+                </div>
 
-              <div className="mt-6">
+                <div>
+                  <div className="text-center">ITEMS</div>
+                  <hr className="border-1 border-[#7D515E]"></hr>
+                </div>
+
                 {order.products.map((productOrdered) => {
-                  return <div>
-                    <div>{productOrdered.product.name}</div>
-                    <div>{productOrdered.product.description}</div>
-                    <img className="w-1/6" src={productOrdered.product.image}></img>
-                    <div>Color: {productOrdered.product.color}</div>
-                    <div>Size: {productOrdered.size}</div>
-                    <div>Quantity: {productOrdered.qty}</div>
-                    <div>Each: {CURRENCY} {productOrdered.price}</div>
-                    <div>Total: {CURRENCY} {productOrdered.product.price * productOrdered.qty}</div>
-
-
-
-
-
+                  return <div className="flex sm:flex-row flex-col sm:gap-16 gap-4">
+                    <div>
+                      <img className="w-[10rem] rounded-md" src={productOrdered.product.image}></img>
+                    </div>
+                    <div>
+                      <div>{productOrdered.product.name}</div>
+                      <hr className="border-1 border-[#7D515E]"></hr>
+                      <div>{productOrdered.product.description}</div>
+                      <div>Color: {productOrdered.product.color}</div>
+                      <div>Size: {productOrdered.size}</div>
+                      <div>Quantity: {productOrdered.qty}</div>
+                      <div>Each: {CURRENCY} {productOrdered.price}</div>
+                      <div>Total: {CURRENCY} {productOrdered.product.price * productOrdered.qty}</div>
+                    </div>
                   </div>
                 })}
+
               </div>
-            </div>
-          })}
+
+            })}
+          </div>
         </div>
         <Footer />
       </div>
