51ee3721f9a272a601f61cf504e4f8fbdccd8b08
Navleen <navleensandhu0211@gmail.com>
2022-12-13 14:41:37 -0800

    displayed message to user in order history page
     - if user is not logged in and visita order history page
       - the UI asks user to first login in order to view their order history

--- src/page_order_history/PageOrderHistory.tsx
+++ src/page_order_history/PageOrderHistory.tsx
@@ -5,6 +5,7 @@ import { useLiveQuery } from "dexie-react-hooks"
 import { useLoggedInUser } from "../hooks/use_logged_in_user"
 import { CURRENCY, product } from "../models/product"
 import { useLoggedInUserOrderHistory } from "../hooks/use_logged_in_user_order_history"
+import { buttonShadowEffect } from "../common/tailwind_constants"
 
 export const PageOrderHistory = () => {
 
@@ -15,7 +16,11 @@ export const PageOrderHistory = () => {
       <div className='text-[#7D515E] flex flex-col min-h-screen'>
         <Header />
         <div className='flex flex-1 flex-col items-center bg-[#F3EBF1]'>
-          {!orderPlacedData?.length && <div className="sm:text-4xl text-xl font-semibold mt-32">You do not have order history!</div>}
+          {!!useLoggedInUser() && !orderPlacedData?.length && <div className="sm:text-4xl text-xl font-semibold mt-64">You do not have order history!</div>}
+          {!useLoggedInUser() && <div className="mt-64 flex flex-col items-center gap-6">
+            <div className="sm:text-4xl text-xl font-semibold text-center">Please login to view your order history</div>
+            <button className={`${buttonShadowEffect} px-4 py-1 bg-[#F4DADB] rounded-md sm:text-3xl text-lg font-semibold`}><a href='/login'>LOGIN</a></button>
+          </div>}
           <div className="flex flex-col gap-8 md:p-16 sm:p-10 p-4 lg:w-1/2 w-full">
             {!!orderPlacedData?.length && orderPlacedData.map((order, i) => {
               return <div key={order.id} className="rounded-md bg-[#F4DADB] flex flex-col lg:gap-6 gap-12 p-4">
