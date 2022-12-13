bff145a2183ec20c1069417a7a001255d0f6b2c9
Navleen <navleensandhu0211@gmail.com>
2022-12-13 14:08:47 -0800

    made Classic Tees font semibold

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <>
       <div className="flex sm:flex-row sm:gap-0 gap-2 flex-col p-6 bg-[#7D515E] sm:justify-between sm:items-start items-center">
-        <a className="text-[#F4DADB] lg:text-3xl md:text-2xl text-lg" href='/'>CLASSIC TEES</a>
+        <a className="text-[#F4DADB] lg:text-3xl md:text-2xl text-lg font-semibold" href='/'>CLASSIC TEES</a>
         <div className="flex flex-col sm:items-start items-center gap-2 text-[#F4DADB] lg:text-2xl md:text-xl text-lg">
           <div className="flex items-center gap-1">© 2022 Navleen Kaur Brar</div>
           <a target="_blank" href="https://www.navleenbrar.com/">Visit My Portfolio</a>
