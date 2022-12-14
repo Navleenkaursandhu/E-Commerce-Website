a0caac4a3a4f4aef2c5a3b8d877ba182965e1185
Navleen <navleensandhu0211@gmail.com>
2022-12-14 09:29:25 -0800

    deleted extra borders on rate us and cx reviews heading

--- src/page_product_details/PageProductDetails.tsx
+++ src/page_product_details/PageProductDetails.tsx
@@ -108,7 +108,7 @@ export const PageProductDetails = () => {
 
         {!!ordersWithProduct?.length && <div className='flex flex-col gap-4 p-2'>
           <div className='mt-10'>
-            <div className='text-center border border-b-[#7D515E]'>RATE US</div>
+            <div className='text-center border border-r-0 border-l-0 border-t-0 border-b-[#7D515E]'>RATE US</div>
             <div className='mt-2'>Please give us a star rating:</div>
             {new Array(5).fill(0).map((element, i) => <button key={i} onClick={() => setStarIndex(i)} className={i <= starIndex ? 'text-yellow-400' : 'text-[#a3838ea3]'}><span className="material-symbols-outlined">
               grade
@@ -135,7 +135,7 @@ export const PageProductDetails = () => {
         </div>}
 
         {!ordersWithProduct?.length && <div>
-          <div className='text-center border border-b-[#7D515E]'>RATE US</div>
+          <div className='text-center border border-r-0 border-l-0 border-t-0 border-b-[#7D515E]'>RATE US</div>
           <div className='mt-6 flex items-center gap-2'>
             <span className="material-symbols-outlined">
               warning
@@ -144,7 +144,7 @@ export const PageProductDetails = () => {
         }
 
         {!!reviewData?.length && <div className='mt-16 p-2'>
-          <div className='text-center border border-b-[#7D515E] mb-6'>CUSTOMER REVIEWS</div>
+          <div className='text-center border border-r-0 border-l-0 border-t-0 border-b-[#7D515E] mb-6'>CUSTOMER REVIEWS</div>
           <div className='flex md:flex-row flex-col gap-6'>
             <div className='px-2'>
               <div>{!!reviewData?.length && reviewData.length} {reviewData.length === 1 ? 'Review' : 'Reviews'}</div>
