4f62d62173cbe9354ef53e106bf72405a0ef8791
Navleen <navleensandhu0211@gmail.com>
2022-12-14 15:52:46 -0800

    added shadow effect upon hover on products listed on main page of the website

--- src/page_main/Body.tsx
+++ src/page_main/Body.tsx
@@ -52,28 +52,31 @@ export const Body = () => {
       <div className='flex flex-wrap px-6 py-16 gap-10 justify-evenly'>
         {sortedProducts.map((obj, i) => {
           const productReviews = !!reviewsData?.length && reviewsData.filter((productReview, i) => productReview.productId === obj.id)
-          console.log(productReviews)
 
           const productReviewsAverage = !!productReviews.length && Math.floor(productReviews.reduce((prevReview, currReview) => prevReview + currReview.rating, 0) / productReviews.length)
-          console.log(productReviewsAverage)
-          return <a href={`/product-details/${obj.id}`} key={obj.id} className='flex flex-col gap-1.5 md:[24rem] sm:w-[16rem] w-[9rem]'>
-            <img className='rounded-md' src={obj.image}></img>
-            <div>
-              {!!productReviewsAverage
-                ? new Array(5).fill(0).map((element, i) => <button key={i} className={i < productReviewsAverage ? 'text-yellow-400' : 'text-[#a3838ea3]'}><span className="material-symbols-outlined">
-                  grade
-                </span></button>)
 
-                : new Array(5).fill(0).map((element, i) => <button key={i} className='text-[#a3838ea3]'><span className="material-symbols-outlined">
-                  grade
-                </span></button>)
+
+          return <div className='hover:shadow-md hover:shadow-rose-200 rounded-md p-2'>
+            <a href={`/product-details/${obj.id}`} key={obj.id} className='flex flex-col gap-1.5 md:[24rem] sm:w-[16rem] w-[9rem]'>
+              <img className='rounded-md' src={obj.image}></img>
+              <div>
+                {!!productReviewsAverage
+                  ? new Array(5).fill(0).map((element, i) => <button key={i} className={i < productReviewsAverage ? 'text-yellow-400' : 'text-[#a3838ea3]'}><span className="material-symbols-outlined">
+                    grade
+                  </span></button>)
+
+                  : new Array(5).fill(0).map((element, i) => <button key={i} className='text-[#a3838ea3]'><span className="material-symbols-outlined">
+                    grade
+                  </span></button>)
+                }
+              </div>
+              <div>{obj.name}</div>
+              <div>{CURRENCY} {obj.price.toFixed(2)}</div>
+              {obj.hasFreeDelivery && <div className='bg-[#F4DADB] sm:px-3 sm:py-1 p-1 rounded-md sm:w-1/2 w-full text-center'>Free Delivery</div>
               }
-            </div>
-            <div>{obj.name}</div>
-            <div>{CURRENCY} {obj.price.toFixed(2)}</div>
-            {obj.hasFreeDelivery && <div className='bg-[#F4DADB] sm:px-3 sm:py-1 p-1 rounded-md sm:w-1/2 w-full text-center'>Free Delivery</div>
-            }
-          </a>
+            </a>
+          </div>
+
         })}
       </div>
     </div>
